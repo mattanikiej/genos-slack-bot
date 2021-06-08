70dc3ccc4047ecaaaa3970de720219fbc53a858a
mattanikiej <mattanikiej@gmail.com>
2021-06-07 22:15:54 -0500

    Please work

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bot/bot:app
+web: gunicorn wsgi:app
--- bot/bot.py
+++ bot/bot.py
@@ -66,7 +66,3 @@ def trivia_command():
 
     # return 200 OK response
     return Response(), 200
-
-
-if __name__ == '__main__':
-    app.run()
